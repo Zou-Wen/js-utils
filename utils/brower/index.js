ba2f80013a2588fc223573ef781050119025896c
zouwen <2871481934@qq.com>
2023-12-18 20:42:52 +0800

    feat: 🎸 增加浏览器相关工具函数

---
+++ utils/brower/index.js
@@ -0,0 +1,19 @@
+/**
+ * 判断当前是否为浏览器环境
+ * @returns {boolean}
+ */
+export function isBrowser() {
+    return typeof window !== 'undefined';
+}
+
+/**
+ * 获取地址栏query参数值
+ * @param {string} name query参数名称
+ * @param {string} search location.search
+ * @returns
+ */
+export function getQueryString(name, search) {
+    const reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    const res = search.substring(1).match(reg);
+    return res !== null ? decodeURIComponent(res[2]) : '';
+}
